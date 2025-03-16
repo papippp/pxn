2dcc7ced805e05702cd78716adfda1b3385e0ff2
papippp <dlukzy@gmail.com>
2025-03-17 00:56:45 +0700

    pic

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import MemeGallery from "./MemeGallery";
+import { Image } from "react-bootstrap";
+
 
 const Hero = () => {
+    const pic = 'https://res.cloudinary.com/dqcztgs4v/image/upload/v1742146412/photo_6248909315687695253_y_yg1tjo.jpg'
     return (
         <section style={{ padding: "5rem", textAlign: "center", background: "#000", color: "#0f0" }}>
             <h1>Welcome to PXN Coin!</h1>
             <p>The meme coin for gamers, powered by Nvidia.</p>
+            <Image src={pic}
+            />
             <div
                 style={{
                     padding: "1rem 2rem",
